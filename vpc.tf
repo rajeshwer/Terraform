70784c9eacc33623979f4d04f230691f24de8251
rajeshwerrao <rajeshwerrao@rajeshwerraos-MacBook-Air.local>
2016-11-20 02:55:27 -0800

    update

---
+++ vpc.tf
@@ -0,0 +1,11 @@
+resource "aws_vpc" "default" {
+    cidr_block = "${var.vpc_cidr}"
+    enable_dns_hostnames = true
+    tags {
+        Name = "terraform-aws-vpc"
+    }
+}
+
+resource "aws_internet_gateway" "default" {
+    vpc_id = "${aws_vpc.default.id}"
+}
